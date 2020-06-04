76f32acbaa65b8d82c8bf9f625b7a1601078e925
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 19:50:22 +0000

    refactored and exported function

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,28 +1,36 @@
 const request = require('request');
-const breedName =  process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+//const breedName =  process.argv.slice(2);
 
-request(url, (error, response, body) => {
-  //console.log('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // Print the HTML for the Google homepage.
 
-  if (error) {
-    console.log("Error: ",error);
-
-  } else {
-    
-    const data = JSON.parse(body);
-    //console.log(typeof)
-    if (data.length === 0) {
-      console.log("Error: requested breed is not found");
+const fetchBreedDescription = function (breedName,callback) {
+  
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(url, (error, response, body) => {
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // Print the HTML for the Google homepage.
+  
+    if (error) {
+      callback(error,null);
+  
     } else {
       
-      console.log(data[0].description);
-      
+      const data = JSON.parse(body);
+      //console.log(typeof)
+      if (data.length === 0) {
+        callback(null,"Error: requested breed is not found");
+      } else {
+        
+        callback(data[0].description);
+        
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = fetchBreedDescription;
+
     
     
     
