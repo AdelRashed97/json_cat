12b96179aa7154802fde84a74507487a6fb73fb1
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 20:06:28 +0000

    deleted unnecesssary comments

--- breedfetcher.js
+++ breedfetcher.js
@@ -7,10 +7,7 @@ const fetchBreedDescription = function(breedName,callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
   request(url, (error, response, body) => {
-    //console.log('error:', error); // Print the error if one occurred
-    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // Print the HTML for the Google homepage.
-  
+    
     if (error) {
       callback(error,null);
   
